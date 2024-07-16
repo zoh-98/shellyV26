0181c821d80d55b9890dc61133d456b4921a0887
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-07-16 20:23:09 +0100

    Update listen.js

--- controler/listen.js
+++ controler/listen.js
@@ -38,7 +38,7 @@ module.exports = function({ threadsData, usersData, globalData , api }) {
     const handleAll = require(__dirname + "/handleAll");
     const handleEvent = require(__dirname + "/handleEvent")({ usersData, sh, threadsData, globalData, api });
     const handleDB = require(__dirname + "/handleDB")({ usersData, threadsData, globalData, api });
-
+    const onListen = require(__dirname + "/onListen");
 
     switch (event.type) {
       case "message":
@@ -48,13 +48,15 @@ module.exports = function({ threadsData, usersData, globalData , api }) {
         handleCmd(params);
         handleReply(params);
         handleAll(params);
+        onListen();
         break;
       case "event":
         handleEvent({ event });
+        onListen();
       if (event.logMessageType) await threadsData.refreshInfo(event.threadID);
         break;
       case "message_reaction":
-
+        onListen();
         // some random react actions
 
         if (event.reaction == "👎" && global.config.AD.includes(event.userID)) {
