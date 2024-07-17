ec2d9e90b3bbeb175dcea16b2e175372b2f79ffe
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-07-17 01:44:19 +0100

    Create cmdB.js

---
+++ Shelly/cmds/cmdB.js
@@ -0,0 +1,67 @@
+let cmd = {
+  config: {
+    name: "امر",
+    Auth: 1, 
+    KJ: ["cmd"],
+    Info: "حظر اوامر من مجموعتك",
+    How: undefined,
+    Hide: false,
+    Time: 0
+  },
+  
+  onType: async function({api, sh, event, threadsData}) {
+    const type = args[0];
+    if (!type) return sh.reply("اكتب حظر او الغاء");
+    switch (type) {
+      case "ban":
+        case "بان":
+          case "حظر":
+          case "-b":
+            if (!args[1]) return sh.reply("اكتب اسم الامر لبدك تبندو");
+            if(!global.shelly.cmds.has(args[1])) return sh.reply("انت بدك تنبد امر مش موجود او كيف");
+            
+            if (!(await threadsData.get(event.threadID)).data.Cban) await threadsData.set(event.threadID, [], "data.Cban");
+            
+            let obj = (await threadsData.get(event.threadID)).data.Cban;
+            
+            if(obj.includes(args[1])) return sh.reply("هذا الامر مبند بالفعل يا خرا");
+            
+            obj.push(args[1]);
+            
+            await threadsData.set(event.threadID , obj , "data.Cban");
+            
+            sh.reply("تم تبنيد امر: " + args[1])
+            
+            break;
+            
+            case "uban":
+        case "الغاء":
+          case "-u":
+            if (!args[1]) return sh.reply("اكتب اسم الامر لبدك تفك البان عنو");
+            let obj = (await threadsData.get(event.threadID)).data.Cban;
+            if (!obj) return sh.reply("ما عندك ولا امر مبند اصلا");
+            if(!obj.includes(args[1])) {
+              return sh.reply("هذا الامر  غير مبند من قبل");
+            }
+            
+            
+            obj.slice(obj.indexOf(args[1]), 1);
+            
+            await threadsData.set(event.threadID , obj , "data.Cban");
+            
+              sh.reply("تم فك تبنيد امر: " + args[1]);
+              
+              break;
+              
+              
+              default: return sh.reply("اكتب بس حظر او الغاء")
+            
+    }
+    
+    
+  }
+}
+
+
+
+module.exports = cmd;
